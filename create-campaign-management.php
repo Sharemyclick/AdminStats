d15dc7b313994e11c4eb9e61a18ad65a35b299bb
usuario <usuario@192.168.1.247>
2014-07-21 12:46:59 +0200

    Merge origin/master
    Conflicts:
    	class/advertiser.class.php

---
+++ create-campaign-management.php
@@ -0,0 +1,223 @@
+<?php
+
+include('conf.php');
+include('class/campaignmanagement.class.php');
+
+include('class/categoryproduct.class.php');
+include('class/country.class.php');
+
+session_start();  
+if(!isset($_SESSION['login'])) {  
+  echo '<script>document.location.href="dashboard.php"</script>';  
+  exit;  
+}
+
+//$objHeadquarter = new AffiliateManager();
+//$resultHeadquarter = $objHeadquarter->getAffiliateManagersList();
+
+
+
+//$objCompany = new AffiliateCompany();
+//$resultCompany = $objCompany->getAffiliateCompanyTable();
+
+ $objCountry = new Country();
+$resultCountry = $objCountry->getCountryList();
+
+$objCategoryProduct = new CategoryProduct();
+$result = $objCategoryProduct->getCategoriesList();
+
+ if(isset($_POST['submit_affiliate_manager'])){
+     
+       $message = $objHeadquarter->createAffiliateManager($_POST);
+     
+ }
+
+?>
+
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Sharemyclick admin platform V1.0</title>
+<link rel="stylesheet" href="css/style.default.css" type="text/css" />
+<link rel="stylesheet" href="prettify/prettify.css" type="text/css" />
+
+<script type="text/javascript" src="prettify/prettify.js"></script>
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.1.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.uniform.min.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="js/jquery.colorbox-min.js"></script>
+<script type="text/javascript" src="js/charCount.js"></script>
+<script type="text/javascript" src="js/targeting.js"></script>
+
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
+</head>
+
+<body>
+
+
+	
+	<?php include ('./menu/menu-left.php');?>
+    
+    <!-- START OF RIGHT PANEL -->
+    <div class="rightpanel">
+    	<div class="headerpanel">
+        	<a href="" class="showmenu"></a>
+            
+            <div class="headerright">
+                
+                <div class="dropdown userinfo">
+                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="/page.html">Hi, <?php echo $_SESSION['login']; ?> <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php"><span class="icon-off"></span> Sign Out</a></li>
+                    </ul>
+                </div><!--dropdown-->
+    		
+            </div><!--headerright-->
+            
+    	</div><!--headerpanel-->
+        <div class="breadcrumbwidget">
+            <ul class="skins">
+                <li><a href="default" class="skin-color default"></a></li>
+                <li><a href="orange" class="skin-color orange"></a></li>
+		<li><a href="green" class="skin-color green"></a></li>
+                <li><a href="dark" class="skin-color dark"></a></li>
+                <li>&nbsp;</li>
+                <li class="fixed"><a href="" class="skin-layout fixed"></a></li>
+                <li class="wide"><a href="" class="skin-layout wide"></a></li>
+            </ul><!--skins-->
+            <ul class="breadcrumb">
+                <li><a href="dashboard.html">Home</a> <span class="divider">/</span></li>
+                <li class="active">Dashboard</li>
+            </ul>
+        </div><!--breadcrumbwidget-->
+        <div class="pagetitle">
+        	<h1>Create Campaigns Management</h1> <span><?php echo $_SESSION['login']; ?> , please enter all the details for the creation of the campaigns management.</span>
+        </div><!--pagetitle-->
+        
+        <div class="maincontent">
+		
+            <div class="contentinner">
+                
+                <?php
+                     if(isset($_POST['submit_affiliate_manager']))
+                         {
+                         ?>   <h4 class='confirmation' style="text-align: center" ">The Campaigns Management has been created </h4> </br> 
+                         <p class="stdformbutton" style="text-align: center">
+                             <a href="create-affiliate-manager.php"
+                             <button type="button" name="create_affiliate_manager_redirection" id="create_affiliate_manager_redirection" class="btn btn-primary" >Create another affiliate manager </button>
+                             </a>
+                             <a href="view-campaigns-management.php"
+                            <button type="button" name="view-campaigns-management" id="view-campaigns-management" class="btn btn-primary">View Campaigns Management </button>
+                             </a>
+                        </p>
+                    <?php ;}
+                Else { ?>
+			<div class="widgetcontent">
+			
+            	<h4 class="widgettitle nomargin shadowed">Campaigns Management informations</h4>
+					
+                <div class="widgetcontent bordered shadowed nopadding">
+                    <form name="form_advertiser" class="stdform stdform2" method="post" action="" enctype="multipart/form-data">
+                        <p>
+                            <label> Name *</label>
+                            <span class="field"><input type="text" name="name" class="input-xxlarge" required="required" /></span>
+                        </p>
+
+                        <p>
+                            <label>Payout for Affiliate *</label>
+                        <span class="field"><input type="text" id="payout_affiliate"  name="payout_affiliate" class="input-xxlarge" required="required" /></span>
+                        </p>
+                        
+                        <p>
+                            <label>Payout for SMC *</label>
+                            <span class="field">
+                               <input type="payout_smc" id="email"  name="payout_smc" class="input-xxlarge" required="required" />
+                            </span>
+                            
+                        </p>
+                             <p>
+                            <label>Type of Payout *</label>
+                            <span class="field">
+                               <input type="text" id="skype"  name="skype" class="input-xxlarge" required="required" />
+                            </span>
+                            
+                        </p>                                 
+                        <p>
+                            <label> Allowed *</label>
+                            <span class="field"><input type="text" name="allowed" class="input-xxlarge" required="required" /></span>
+                        </p>
+                                         
+			<p>
+                            <label>Category</label>
+                            
+                           <span class="field">
+                                <select name="id_category_product" id="id_category_product" class="status">
+                                        <?php 
+                                        foreach($objCategoryProduct->categories_list as $indCat => $valCat){?>
+                                    
+                                        
+                                    <option value="<?php echo $valCat['id']; ?>" <?php if($valCat['type']==0){?> class="option"   style="color:black;" <?php } else {?> style="text-align:center; color:grey;" <?php }?> > <?php if($valCat['type']==0){} else { ?>&nbsp &nbsp &nbsp  <?php        } echo $valCat['name']; ?></option>
+                                            </option>
+                                <?php } ?>
+                                </select>
+                            </span>  
+                            
+                        </p>
+                         <p>
+                            <label>Country *</label>
+                            <span class="field">
+                                <select name="id_country" id="id_country" class="status">
+                                        <?php 
+                                        foreach($objCountry->countryselect as $indCountry => $valCountry){?>
+                                        
+                                    <option value="<?php echo $valCountry['id_country']; ?>"  ><?php echo $valCountry['name_country']; ?> </option>
+                                            </option>
+                                <?php } ?>
+                                </select>
+                            </span>
+                            
+                        </p>
+                        <p>
+                            <label> Thumbnail *</label>
+                            <span class="field">
+                                
+                                <input type="text" name="date_birth" class="status" required="required" />
+                            </span>
+                            </p>
+                        
+                        
+                      
+                        
+                        <p class="stdformbutton">
+                            <button type="submit" name="submit_affiliate_manager" id="submit_affiliate_manager" class="btn btn-primary">Create </button>
+                            <button type="reset" class="btn">Reset </button>
+                        </p>
+                        
+                        </form>
+                    </div>				
+                </div><!--contentinner-->
+                </div><!--contentinner--> <?php }; ?>
+        </div><!--maincontent-->
+        
+    </div><!--rightpanel-->
+    <!-- END OF RIGHT PANEL -->
+    
+    <div class="clearfix"></div>
+	
+
+	
+    <div class="footer">
+    	<div class="footerleft">Sharemyclick dashboard v1.0</div>
+    	<div class="footerright">&copy; Sharemyclick with Themepixels - <a href="https://twitter.com/sharemyclick"><span class="iconsweets-twitter"></a> - <a href="https://www.facebook.com/sharemyclick"><span class="iconsweets-facebook"></a> - <a href="https://www.linkedin.com/company/sharemyclick">Followus on Linkedin</a></div>
+    </div><!--footer-->
+ 
+
+
+</body>
+</html>
+}
