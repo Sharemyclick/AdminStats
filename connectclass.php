91fb1e6250df551b611d68e6f0f745a204b52868
usuario <usuario@192.168.1.247>
2014-07-28 09:55:37 +0200

    Merge origin/master
    Conflicts:
    	class/advertiser.class.php

---
+++ connectclass.php
@@ -0,0 +1,12 @@
+<?php
+
+include('class/advertiser.class.php');
+include('class/affiliatecompany.class.php');
+include('class/affiliatemanager.class.php');
+include('class/campaigncontract.class.php');
+include('class/campaignmanagement.class.php');
+include('class/categoryproduct.class.php');
+include('class/country.class.php');
+include('class/database.class.php');
+include('class/user.class.php');
+?>
